7bd61bf72e3b275f545e478cf9e13ec12d0c6048
Chiemerie Valentine <chiemerieezechukwu00@gmail.com>
2024-07-11 20:25:45 +0100

    jenkins-Docker

--- terraform/development/public_subnet.tf
+++ terraform/development/public_subnet.tf
@@ -5,7 +5,7 @@ module "public_subnet" {
   vpc_id            = module.vpc.vpc_id
   availability_zone = module.data.availability_zone[0]
   cidr_block        = lookup(var.subnet_cidr, "public")
-  open_ports        = [80, 443, 3000, 3100, 4000, 5000, 9615, 9080, 9090, 9100, 9273, 8080, var.SSH_PORT]
+  open_ports        = [514, 80, 443, 3000, 3100, 4000, 5000, 9615, 9080, 9090, 9100, 9273, 8080, var.SSH_PORT]
   depends_on        = [module.vpc]
   allowed_IPs       = [var.ALL_IPs]
   tags = {
